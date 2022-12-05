22c6914a3186709b2eac6a42d5bc6013975c0c11
febqij <ko2l0v.pav@yandex.ru>
2022-12-05 10:14:44 +0700

    initial commit

---
+++ dndgram/bot.py
@@ -0,0 +1,18 @@
+import asyncio
+import logging
+
+
+async def on_startup(dp):
+    from utils.notify_admins import on_startup_notify
+    await on_startup_notify(dp)
+
+    from utils.set_bot_commands import set_default_commands
+    await set_default_commands(dp)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+    from aiogram import executor
+    from handlers import dp
+
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
